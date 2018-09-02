7e428ddb51bef20e0953d68df33b367b3755f82a
7NZ <weiruan552406@live.com>
2018-09-02 16:54:19 +0800

    'push'

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 // export let apiRoot = 'http://127.0.0.114/wp-json/wp/v2'
-export let apiRoot = '/wp-json/wp/v2/'
+export let apiRoot = '/wp-json/wp/v2'
 
 // export default {
 //   apiRoot: 'http://127.0.0.114/wp-json/wp/v2'
