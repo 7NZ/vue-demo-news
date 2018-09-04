c84feeba0d2753c8c41ee08aacd86fa20a6d4180
7NZ <weiruan552406@live.com>
2018-09-04 15:25:50 +0800

    '更新gitignore'

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,12 @@
 // export let apiRoot = 'http://127.0.0.114/wp-json/wp/v2'
-export let apiRoot = '/wp-json/wp/v2'
+// export let apiRoot = '/wp-json/wp/v2'
 
-// export default {
-//   apiRoot: 'http://127.0.0.114/wp-json/wp/v2'
-// }
+let apiRoot
+
+if (process.env.NODE_ENV === 'development') {
+  apiRoot = '/wp-json/wp/v2'
+} else {
+  apiRoot = 'https://www.wpdaxue.com/wp-json/wp/v2'
+}
+
+export {apiRoot}
